156a72e5f2265b131182cbc08093d3c24d10e265
Dylan King <dwkblue51@yahoo.com>
2021-10-06 17:49:20 -0500

    begin building ship factory function

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,13 @@
-function ship(){
-    
+function ship(length){
+    const health = [];
+    // fill health array
+    for (let i = 0; i < length; i++) {
+        health.push("");
+    };
+    const hit = () => {
+
+    }
+    const isSunk = () => {
+
+    }
 }
