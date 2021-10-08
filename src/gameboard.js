fd9e1d7dd6a4ea3fc5e2dac502a72071b99033ef
Dylan King <dwkblue51@yahoo.com>
2021-10-08 14:58:27 -0500

    gameboard creates ships using ship factory function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,18 @@
-const gameBoard = () => {
+const gameBoard = (shipFunc, name) => {
     // gameboard places ships at coordinates
     // use array to create board
     const board = [];
     for (let i = 0; i < 100; i++) {
         board.push('');
     };
-    return {board};
+    const ships = {
+        carrier : shipFunc(5),
+        battleship : shipFunc(4),
+        destroyer : shipFunc(3),
+        submarine : shipFunc(3),
+        patrol : shipFunc(2),
+    };
+    return {board, name, ships};
 };
 
 export default gameBoard;
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,6 +1,16 @@
 import gb from './gameboard';
+import ship from './ship';
 
 test("gameboard returns correct number of grid squares", () => {
     const player = gb();
     expect(player.board.length).toBe(100);
 });
+
+test("gameboard correctly creates ships", () => {
+    const player = gb(ship, player);
+    expect(player.ships.carrier.health.length).toBe(5);
+});
+
+test.skip("gameboard places ships horizontally on correct squares", () => {
+
+})
