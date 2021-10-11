f15bfea81bb70654a4958ef503a8c2fc2312ac2c
Dylan King <dwkblue51@yahoo.com>
2021-10-11 15:34:46 -0500

    format files

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,54 +1,66 @@
-import ship from './ship';
+import ship from "./ship";
 
 const gameBoard = (name) => {
-    const board = [];
-    for (let i = 0; i < 100; i++) {
-        board.push('');
-    };
-    const carrier = ship(5);
-    const battleship = ship(4);
-    const destroyer = ship(3);
-    const submarine = ship(3);
-    const patrol = ship(2);
-    const placeShip = (direction, coordinate, ship) => {
-        let marker;
-        switch (ship) {
-            case carrier:
-                marker = 'c';
-                break;
-            case battleship:
-                marker = 'b';
-                break;
-            case destroyer:
-                marker = 'd';
-                break;
-            case submarine:
-                marker = 's';
-                break;
-            case patrol:
-                marker = 'p';
-                break;
-        
-            default:
-                break;
-        };
-        if (direction === 'horizontal') {
-            // check to see if ship placement does not exceed edge of gameboard
-            const coordString = coordinate.toString();
-            if ((parseInt(coordString[coordString.length - 1]) + ship.health.length) <= 9) {
-                for (let i = 0; i < ship.health.length; i++) {
-                    board.splice(coordinate + i, 1, marker);
-                };
-                return board;
-            } else {
-                throw new Error ("illegal move. ship cannot pass edge of board!");
-            };
-        } else {
-            // temoparily return while testing horizontal placement
-            return;
-        }
+  const board = [];
+  for (let i = 0; i < 100; i++) {
+    board.push("");
+  }
+  const carrier = ship(5);
+  const battleship = ship(4);
+  const destroyer = ship(3);
+  const submarine = ship(3);
+  const patrol = ship(2);
+  const placeShip = (direction, coordinate, ship) => {
+    let marker;
+    switch (ship) {
+      case carrier:
+        marker = "c";
+        break;
+      case battleship:
+        marker = "b";
+        break;
+      case destroyer:
+        marker = "d";
+        break;
+      case submarine:
+        marker = "s";
+        break;
+      case patrol:
+        marker = "p";
+        break;
+
+      default:
+        break;
     }
-    return {board, name, carrier, battleship, destroyer, submarine, patrol, placeShip};
+    if (direction === "horizontal") {
+      // check to see if ship placement does not exceed edge of gameboard
+      const coordString = coordinate.toString();
+      if (
+        parseInt(coordString[coordString.length - 1]) + ship.health.length <=
+        9
+      ) {
+        for (let i = 0; i < ship.health.length; i++) {
+          board.splice(coordinate + i, 1, marker);
+        }
+        return board;
+      } else {
+        throw new Error("illegal move. ship cannot pass edge of board!");
+      }
+    } else {
+      // temoparily return while testing horizontal placement
+      return;
+    }
+  };
+  return {
+    board,
+    name,
+    carrier,
+    battleship,
+    destroyer,
+    submarine,
+    patrol,
+    placeShip,
+  };
 };
 
 export default gameBoard;
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,34 +1,33 @@
-import gb from './gameboard';
-import ship from './ship';
+import gb from "./gameboard";
+import ship from "./ship";
 
 test("gameboard returns correct number of grid squares", () => {
-    const player = gb();
-    expect(player.board.length).toBe(100);
+  const player = gb();
+  expect(player.board.length).toBe(100);
 });
 
 test("gameboard correctly creates ships", () => {
-    const player = gb(player);
-    expect(player.carrier.health.length).toBe(5);
+  const player = gb(player);
+  expect(player.carrier.health.length).toBe(5);
 });
 
 describe("gameboard places ships correctly", () => {
-    test("gameboard places ships horizontally starting at 0", () => {
-        const player = gb(player);
-        player.placeShip('horizontal', 0, player.carrier);
-        expect(player.board.slice(0, 5)).toStrictEqual(['c', 'c', 'c', 'c', 'c']);
-    });
+  test("gameboard places ships horizontally starting at 0", () => {
+    const player = gb(player);
+    player.placeShip("horizontal", 0, player.carrier);
+    expect(player.board.slice(0, 5)).toStrictEqual(["c", "c", "c", "c", "c"]);
+  });
 
-    test("gameboard places ships horizontally starting at 1", () => {
-        const player = gb(player);
-        player.placeShip('horizontal', 1, player.carrier);
-        expect(player.board.slice(1, 6)).toStrictEqual(['c', 'c', 'c', 'c', 'c']);
-    });
-    
-    test("gameboard.placeShip throws error if ship exceeds edge of board", () => {
-        const player = gb(player);
-        expect(() => player.placeShip('horizontal', 5, player.carrier)).toThrow();
-        expect(() => player.placeShip('horizontal', 6, player.carrier)).toThrow();
-        expect(() => player.placeShip('horizontal', 36, player.carrier)).toThrow();
-    });
-    
-})
+  test("gameboard places ships horizontally starting at 1", () => {
+    const player = gb(player);
+    player.placeShip("horizontal", 1, player.carrier);
+    expect(player.board.slice(1, 6)).toStrictEqual(["c", "c", "c", "c", "c"]);
+  });
+
+  test("gameboard.placeShip throws error if ship exceeds edge of board", () => {
+    const player = gb(player);
+    expect(() => player.placeShip("horizontal", 5, player.carrier)).toThrow();
+    expect(() => player.placeShip("horizontal", 6, player.carrier)).toThrow();
+    expect(() => player.placeShip("horizontal", 36, player.carrier)).toThrow();
+  });
+});
