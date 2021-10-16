cc3adf5e7a8fd05fcfd6f89ca9f5765525a3a51e
Dylan King <dwkblue51@yahoo.com>
2021-10-15 21:09:52 -0500

    create grid-gen.js and hardcode ship placement in index.js

---
+++ src/grid-gen.js
@@ -0,0 +1,13 @@
+const gridGen = (num) => {
+    const container = document.createElement('div');
+    for(let i = 0; i < num; i++){
+        let grid = document.createElement('div');
+        grid.classList.add('grid-item');
+        grid.dataset.id = i;
+        container.appendChild(grid);
+
+    };
+    return container;
+};
+
+export default gridGen;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import gb from './gameboard';
 import ship from './ship';
 import player from './player';
+import grid from './grid-gen';
 
 (() => {
     const player1 = player(player1);
     const playerBoard = gb(playerBoard);
     const computer = player(computer);
     const computerBoard = gb(computerBoard);
+    const display = document.createElement('div');
+    document.body.appendChild(display);
+    display.id = 'display';
+    display.append(grid(playerBoard.board.length), grid(computerBoard.board.length));
+    // temporarily hardcode ships into place
+    playerBoard.placeShip('horizontal', 0, playerBoard.carrier);
+    playerBoard.placeShip('horizontal', 10, playerBoard.battleship);
+    playerBoard.placeShip('horizontal', 20, playerBoard.destroyer);
+    playerBoard.placeShip('horizontal', 30, playerBoard.submarine);
+    playerBoard.placeShip('horizontal', 40, playerBoard.patrol);
+    computerBoard.placeShip('horizontal', 0, computerBoard.carrier);
+    computerBoard.placeShip('horizontal', 10, computerBoard.battleship);
+    computerBoard.placeShip('horizontal', 20, computerBoard.destroyer);
+    computerBoard.placeShip('horizontal', 30, computerBoard.submarine);
+    computerBoard.placeShip('horizontal', 40, computerBoard.patrol);
     // main loop
     while(!playerBoard.shipsSunk && !computerBoard.shipsSunk) {
-        
+        // set first player to make first move
+        if (!player1.isTurn && !computer.isTurn) {
+            player1.isTurn = true;
+        }
+        if (player1.isTurn) {
+            computerBoard.board.forEach((item) => {
+                item.addEventListener('click', () => {
+                    computerBoard.receiveAttack(item.dataset.id);
+                });
+            })
+        }
     }
 })();
