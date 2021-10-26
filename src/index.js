5428454f63998fb7d0b90c92cdd3c222a0579c78
Dylan King <dwkblue51@yahoo.com>
2021-10-26 09:49:27 -0500

    replace equality operators with assignment operators, add .toLowerCase to fix bug on placeShip function

--- src/index.js
+++ src/index.js
@@ -43,8 +43,8 @@ const game = (() => {
     const play = () => {
         const directionBtn = document.getElementById('direction');
         directionBtn.addEventListener('click', () => {
-            directionBtn.textContent === 'Horizontal' ? directionBtn.textContent === 'Vertical'
-            : directionBtn.textContent === 'Horizontal'
+            directionBtn.textContent === 'Horizontal' ? directionBtn.textContent = 'Vertical'
+            : directionBtn.textContent = 'Horizontal'
         })
         // grab all player 1 squares for computer to select
         let nodelist = document.querySelectorAll('div.player-grid');
@@ -58,28 +58,29 @@ const game = (() => {
         }
         let playerShips = 0;
         document.addEventListener('click', function el(e) {
+            // let direction = directionBtn.textContent
             // wait until ships are placed before continuing
             const shipList = [playerBoard.carrier, playerBoard.battleship, playerBoard.destroyer, playerBoard.submarine, playerBoard.patrol];
             if (e.target && e.target.classList.contains('player-grid') && playerShips < 5) {
                 switch (playerShips) {
                     case 0:
-                        playerBoard.placeShip(directionBtn.textContent, parseInt(e.target.dataset.id), playerBoard.carrier);
+                        playerBoard.placeShip(directionBtn.textContent.toLowerCase(), parseInt(e.target.dataset.id), playerBoard.carrier);
                         playerShips++;
                         break;
                     case 1:
-                        playerBoard.placeShip(directionBtn.textContent, parseInt(e.target.dataset.id), playerBoard.battleship);
+                        playerBoard.placeShip(directionBtn.textContent.toLowerCase(), parseInt(e.target.dataset.id), playerBoard.battleship);
                         playerShips++;
                         break;
                     case 2:
-                        playerBoard.placeShip(directionBtn.textContent, parseInt(e.target.dataset.id), playerBoard.destroyer);
+                        playerBoard.placeShip(directionBtn.textContent.toLowerCase(), parseInt(e.target.dataset.id), playerBoard.destroyer);
                         playerShips++;
                         break;
                     case 3:
-                        playerBoard.placeShip(directionBtn.textContent, parseInt(e.target.dataset.id), playerBoard.submarine);
+                        playerBoard.placeShip(directionBtn.textContent.toLowerCase(), parseInt(e.target.dataset.id), playerBoard.submarine);
                         playerShips++;
                         break;
                     case 4:
-                        playerBoard.placeShip(directionBtn.textContent, parseInt(e.target.dataset.id), playerBoard.patrol);
+                        playerBoard.placeShip(directionBtn.textContent.toLowerCase(), parseInt(e.target.dataset.id), playerBoard.patrol);
                         playerShips++;
                         break;
                 
